580ab21d25c31a38ea949b707dfa60fba297feab
César Alejandro Moltoni <cesarmoltoni@gmail.com>
2023-03-20 18:55:43 -0300

    Improved Player movement and collision shape

--- Player.gd
+++ Player.gd
@@ -1,5 +1,8 @@
 extends KinematicBody2D
 
+const MAX_SPEED = 100
+const ACCEL = 3.0
+
 var velocity = Vector2()
 
 func _physics_process(delta):
@@ -9,8 +12,8 @@ func _physics_process(delta):
 	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	
 	if input_vector.length_squared():
-		velocity = input_vector
+		velocity = velocity.move_toward(input_vector.normalized() * MAX_SPEED, ACCEL)
 	else:
-		velocity = Vector2()
+		velocity = velocity.move_toward(Vector2(), ACCEL * 2.0)
 	
-	move_and_collide(velocity)
+	move_and_collide(velocity * delta)
