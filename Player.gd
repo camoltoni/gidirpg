8b498e5cdb9bc8461273fc182466f3f23db9414c
César Alejandro Moltoni <cesarmoltoni@gmail.com>
2023-03-27 10:25:17 -0300

    Added animation tree for player

--- Player.gd
+++ Player.gd
@@ -11,9 +11,15 @@ func _physics_process(_delta):
 	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	
+	input_vector = input_vector.normalized()
+	
 	if input_vector.length_squared():
-		velocity = velocity.move_toward(input_vector.normalized() * MAX_SPEED, ACCEL)
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCEL)
+		$AnimTree.set("parameters/Idle/blend_position", input_vector)
+		$AnimTree.set("parameters/Walk/blend_position", input_vector)
+		$AnimTree["parameters/playback"].travel("Walk")
 	else:
 		velocity = velocity.move_toward(Vector2(), ACCEL * 2.0)
+		$AnimTree["parameters/playback"].travel("Idle")
 	
 	velocity = move_and_slide(velocity)
